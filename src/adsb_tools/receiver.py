7057c4a493c7f0cc407dbd80a597c790347331a5
herereadthis <herereadthis.github@gmail.com>
2023-03-24 20:29:36 -0400

    receiver

--- src/adsb_tools/receiver.py
+++ src/adsb_tools/receiver.py
@@ -1,10 +1,14 @@
-import json
-import requests
+from adsb_tools.utils import requests_utils
 
-def get_receiver(base_url):
-    receiver_url = f'{base_url}/data/receiver.json'
-
-    response = requests.get(receiver_url)
-    json_obj = json.loads(response.content)
-
-    return json_obj
+class Receiver:
+    def __init__(self, base_adsb_url):
+        self.url = f'{base_adsb_url}/data/receiver.json'
+        receiver = requests_utils.get_api(self.url)
+        self.version = receiver['version']
+        self.refresh = receiver['refresh']
+        self.history = receiver['history']
+        self.version = receiver['version']
+        self.lat = receiver['lat']
+        self.lon = receiver['lon']
+        self.latitude = receiver['latitude']
+        self.longitude = receiver['longitude']
